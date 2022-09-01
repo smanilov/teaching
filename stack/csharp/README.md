bbf333d7d491ab2260bd716ac4b552a934de8fc4
Stan Manilov <stanislav.manilov@gmail.com>
2022-09-01 14:11:56 +0300

    Add more tests and solution

---
+++ stack/csharp/README.md
@@ -0,0 +1,11 @@
+To run with Mono:
+
+```
+mcs TestStack.cs MyStack.cs IntegerStack.cs && mono TestStack.exe
+```
+
+or 
+
+```
+mcs TestStack.cs solution/MyStack.cs IntegerStack.cs && mono TestStack.exe
+```
---
+++ stack/csharp/TestStack.cs
@@ -0,0 +1,106 @@
+using System;
+
+/// Tests implementations of IntegerStack.
+class TestStack {
+  static void testStack_isLifo() {
+    IntegerStack s = new MyStack();
+
+    s.push(3);
+    s.push(1);
+    s.push(4);
+
+    testEqual(s.pop(), 4);
+    testEqual(s.pop(), 1);
+    testEqual(s.pop(), 3);
+  }
+
+  static void testStackPeek_returnsLastPushed() {
+    IntegerStack s = new MyStack();
+
+    s.push(3);
+    testEqual(s.peek(), 3);
+
+    s.push(1);
+    testEqual(s.peek(), 1);
+
+    s.push(4);
+    testEqual(s.peek(), 4);
+  }
+
+  static void testStackPeek_doesNotRemoveElements() {
+    IntegerStack s = new MyStack();
+
+    s.push(3);
+    s.push(1);
+    s.push(4);
+
+    testEqual(s.peek(), 4);
+    testEqual(s.peek(), 4);
+    testEqual(s.peek(), 4);
+  }
+
+  static void testStack_poppingEmptyThrowsException_1() {
+    IntegerStack s = new MyStack();
+    bool testPassed = false;
+    try {
+      s.pop();
+    } catch (Exception) {
+      testPassed = true;
+    }
+
+    if (!testPassed) {
+      throw new Exception("Calling stack.pop() on an empty stack did not throw an exception."); 
+    }
+  }
+
+  static void testStack_peekingEmptyThrowsException_1() {
+    IntegerStack s = new MyStack();
+    bool testPassed = false;
+    try {
+      s.peek();
+    } catch (Exception) {
+      testPassed = true;
+    }
+
+    if (!testPassed) {
+      throw new Exception("Calling stack.peek() on an empty stack did not throw an exception."); 
+    }
+  }
+
+  static void testEqual(int a, int b) {
+    if (a != b) {
+      throw new Exception("Expected {b}, but got {a}.");
+    }
+  }
+
+  static void testStack() {
+    testStack_isLifo();
+    testStackPeek_returnsLastPushed();
+    testStackPeek_doesNotRemoveElements();
+
+    printGreen("Success!");
+  }
+
+  static void Main(string[] args) {
+    try {
+      testStack();
+    } catch (Exception e) {
+      printRed("Fail");
+      throw e;
+    }
+  }
+
+  static void printRed(String text) {
+    ConsoleColor previous = Console.ForegroundColor;
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine(text);
+    Console.ForegroundColor = previous;
+  }
+
+  static void printGreen(String text) {
+    ConsoleColor previous = Console.ForegroundColor;
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine(text);
+    Console.ForegroundColor = previous;
+  }
+}
